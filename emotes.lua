3059599d5e7dedfb37931d10aec29a1f395bad1c
Kaja <riotakira@yahoo.co.jp>
2024-12-03 20:25:56 +0100

    added AAAA

--- emotes.lua
+++ emotes.lua
@@ -223,4 +223,5 @@ judhead_emotes = {
     ["dankG"] = basePath .. "dankG.tga:56:28",
     ["Bruheg"] = basePath .. "Bruheg.tga:28:28",
     ["Bruhge"] = basePath .. "Bruhge.tga:28:28",
+    ["AAAA"] = basePath .. "AAAA.tga:28:28",
 }
