60c624fb7d7f37b86fe874f0e946964a9ca113db
Kaja <riotakira@yahoo.co.jp>
2024-12-05 15:05:19 +0100

    added emotes to changelog

--- release.sh
+++ release.sh
@@ -35,7 +35,7 @@ gh release create \
     "$version" \
     --verify-tag \
     --title "TwitchEmotes Hardcore $version" \
-    --notes "TwitchEmotes Hardcore $version - added bongTime, jointTime, Lamonting" \
+    --notes "TwitchEmotes Hardcore $version - added bongTime, jointTime, Lamonting, Nerd, Wankge, peepoFine, FeelsWeakMan" \
     "dist/TwitchEmotes_Hardcore-$version.zip"
 
 echo "Published version $version to GitHub"
@@ -45,7 +45,7 @@ echo "Published version $version to GitHub"
 
 metadata=$(cat <<EOF
 {
-    "changelog": "$version added bongTime, jointTime, Lamonting",
+    "changelog": "$version added bongTime, jointTime, Lamonting, Nerd, Wankge, peepoFine, FeelsWeakMan",
     "displayName": "TwitchEmotes Hardcore $version",
     "gameVersions": [12216, 12215, 11925, 10272],
     "releaseType": "$stage"
