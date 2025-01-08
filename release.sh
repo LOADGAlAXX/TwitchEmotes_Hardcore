2570758a34affec06ca64077485c0d0ecde16464
Kaja <riotakira@yahoo.co.jp>
2025-01-08 12:36:51 +0100

    updated changelog

--- release.sh
+++ release.sh
@@ -35,7 +35,7 @@ gh release create \
     "$version" \
     --verify-tag \
     --title "TwitchEmotes Hardcore $version" \
-    --notes "TwitchEmotes Hardcore $version - added 59, 60" \
+    --notes "TwitchEmotes Hardcore $version - added FINALLY, badvibes edited 59, 60" \
     "dist/TwitchEmotes_Hardcore-$version.zip"
 
 echo "Published version $version to GitHub"
@@ -45,7 +45,7 @@ echo "Published version $version to GitHub"
 
 metadata=$(cat <<EOF
 {
-    "changelog": "$version added 59, 60",
+    "changelog": "$version added FINALLY, badvibes edited 59, 60",
     "displayName": "TwitchEmotes Hardcore $version",
     "gameVersions": [12216, 12215, 11925, 10272],
     "releaseType": "$stage"
